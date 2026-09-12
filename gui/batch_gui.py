b13dad79bc0262f624d3a6ed047b8e56dba7f3a8
Frank Igbokwe <frankigbokwe@gmail.com>
2026-09-12 03:37:55 -0400

    Add GUI assets and batch_gui.py application files

---
+++ gui/batch_gui.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+====================================================================
+Project Name : Honeypot_Omaha (Unified Honeypot Analysis Pipeline)
+Author       : Frank Ngoka Igbokwe
+Created      : 2026
+Copyright    : (c) 2026 Frank Ngoka Igbokwe. All rights reserved.
+License      : Business Source License 1.1 (BSL-1.1)
+====================================================================
+"""
+
+
+
+
+#Coming Soon.
